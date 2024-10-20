462a1531ad44808f2475b1186e5fd749a0c80dcf
Ítalo Braga <italo@orbesoft.com.br>
2024-10-20 17:16:58 -0300

    chore: initial configs for angular mfe

---
+++ angular-mfe/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { APP_BASE_HREF } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
